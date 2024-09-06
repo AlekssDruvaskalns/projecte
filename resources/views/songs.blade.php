4c351837c8fae2d95b3d03596344892c79c55c88
unknown <you@example.com>
2024-09-06 13:06:43 +0300

    tukans

--- resources/views/songs.blade.php
+++ resources/views/songs.blade.php
@@ -1,3 +1,25 @@
-<div>
-    <p>asdasdasddasasdasdasd</p>
-</div>
+<x-layout>
+    <h1>Songs</h1>
+
+    <div>
+        {{ session('message') }}
+    </div>
+
+    <a href="{{ route('playlist.create') }}">create</a>
+    <div>
+        @foreach($songs as $song)
+            <p>{{ Str::words($song->song, 30) }}</p>
+            <a href="{{ route('playlist.show', $song->id) }}">Show</a>
+            <a href="{{ route('playlist.edit', $song->id) }}">Edit</a>
+            <a href="#">Show</a>
+            <form action="{{ route( 'playlist.destroy' , $song->id) }}" method="post">
+                @csrf
+                @method('delete');
+                <button type="submit">Delete</button>
+            </form>
+
+            
+        @endforeach
+    </div>
+</x-layout>
+
